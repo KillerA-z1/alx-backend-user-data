02cbd747e14168d09b8650424283c83f37bc28f4
KillerA-z1 <ahmedz1a2m@gmail.com>
2024-11-13 02:02:30 +0300

    Task 6.Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Basic authentication module for the API."""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class."""
+    pass
