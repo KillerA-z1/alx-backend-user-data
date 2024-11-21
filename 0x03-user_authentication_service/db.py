e428be1a4ee2fdcf415b3502af5f61c49eef153f
KillerA-z1 <ahmedz1a2m@gmail.com>
2024-11-21 23:55:36 +0300

    Task 2.Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -37,9 +39,30 @@ class DB:
             is not provided
         """
         if not email or not hashed_password:
-            return
+            return None
         user = User(email=email, hashed_password=hashed_password)
         session = self._session
         session.add(user)
         session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arguments
+
+        Args:
+            kwargs (dict): arbitrary keyword arguments
+
+        Returns:
+            User: user found
+
+        Raises:
+            NoResultFound: if no user is found
+            InvalidRequestError: if invalid query arguments are passed
+        """
+        session = self._session
+        try:
+            return session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound("No user found with the given criteria")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments")
