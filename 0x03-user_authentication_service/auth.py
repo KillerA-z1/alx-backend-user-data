1dcd04ef99d1bea82f3a6b64315446fd62594403
KillerA-z1 <ahmedz1a2m@gmail.com>
2024-11-22 20:17:32 +0300

    Task 18.Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -143,3 +143,25 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update a user's password using the reset token.
+
+        Args:
+            reset_token (str): The reset token provided for password reset.
+            password (str): The new password to set.
+
+        Raises:
+            ValueError: If the reset token is invalid.
+        """
+        if reset_token is None or password is None:
+            raise ValueError("Invalid reset token or password")
+
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError("Invalid reset token")
+
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed_password,
+                             reset_token=None)
